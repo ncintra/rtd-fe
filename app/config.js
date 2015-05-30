48dc8ddf6a1e1b5fed3e9e27355ff8219574cc97
ncintra <ncintra1982@gmail.com>
2015-05-30 23:49:53 +0100

    first upload

---
+++ app/config.js
@@ -0,0 +1,7 @@
+System.config({
+    'map': {
+        'mithril': 'node_modules/mithril/mithril.min',
+        'polythene': 'node_modules/polythene/lib/polythene/polythene',
+        'polythene-theme': 'node_modules/polythene/lib/theme/theme'
+    }
+});
