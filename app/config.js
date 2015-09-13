b1ffe85915695f4224f9ee60005ba204e7a9143b
ncintra <ncintra1982@gmail.com>
2015-09-13 17:25:09 +0100

    try this again

--- app/config.js
+++
@@ -1,7 +0,0 @@
-System.config({
-    'map': {
-        'mithril': 'node_modules/mithril/mithril.min',
-        'polythene': 'node_modules/polythene/lib/polythene/polythene',
-        'polythene-theme': 'node_modules/polythene/lib/theme/theme'
-    }
-});
